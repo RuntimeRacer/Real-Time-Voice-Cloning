7ea2182cb0cfc3296eea613299a776ff410ad158
RuntimeRacer <runtimeracer@gmail.com>
2022-07-25 01:58:58 +0200

    add skip existing param to embedding creation

--- synthesizer/preprocess.py
+++ synthesizer/preprocess.py
@@ -311,7 +311,7 @@ def create_embeddings(synthesizer_root: Path, encoder_model_fpath: Path, skip_ex
     if skip_existing:
         embedding_files = list(embed_dir.glob("embed-*.npy"))
         print(embedding_files[0])
-        utterance_ids[:] = (utterance_id for utterance_id in utterance_ids if not str("embed-%s.npy" % utterance_id) in embedding_files)
+        utterance_ids[:] = (utterance_id for utterance_id in utterance_ids if not str(embed_dir.joinpath("embed-%s.npy" % utterance_id)) in embedding_files)
 
     # TODO: improve on the multiprocessing, it's terrible. Disk I/O is the bottleneck here.
     # Embed the utterances in separate threads
