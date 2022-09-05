78196d613d557bdc3a2e5c629b2d541e435c7ce6
RuntimeRacer <runtimeracer@gmail.com>
2022-09-05 02:06:57 +0200

    Adaptions for Vocoder inference methods to be capable of handling the geneing model

--- vocoder/inference.py
+++ vocoder/inference.py
@@ -1,49 +1,45 @@
-from vocoder.models.fatchord_version import WaveRNN
-from config.hparams import wavernn_fatchord as hp_wavernn, sp
+
+from config.hparams import sp, wavernn_fatchord, wavernn_geneing
+from vocoder.models import base
 import torch
 
 
-_model = None   # type: WaveRNN
+_model = None
+_model_type = None
+
 
 def load_model(weights_fpath, verbose=True):
     global _model, _device
-    
-    if verbose:
-        print("Building Wave-RNN")
-    _model = WaveRNN(
-        rnn_dims=hp_wavernn.rnn_dims,
-        fc_dims=hp_wavernn.fc_dims,
-        bits=hp_wavernn.bits,
-        pad=hp_wavernn.pad,
-        upsample_factors=hp_wavernn.upsample_factors,
-        feat_dims=sp.num_mels,
-        compute_dims=hp_wavernn.compute_dims,
-        res_out_dims=hp_wavernn.res_out_dims,
-        res_blocks=hp_wavernn.res_blocks,
-        hop_length=sp.hop_size,
-        sample_rate=sp.sample_rate,
-        mode=hp_wavernn.mode
-    )
 
     if torch.cuda.is_available():
-        _model = _model.cuda()
         _device = torch.device('cuda')
     else:
         _device = torch.device('cpu')
+
+    # Load model weights from provided model path
+    checkpoint = torch.load(weights_fpath, map_location=_device)
+    model_type = base.MODEL_TYPE_FATCHORD
+    if "model_type" in checkpoint:
+        model_type = checkpoint["model_type"]
+
+    # Init the model
+    try:
+        _model, _ = base.init_voc_model(model_type, _device)
+        _model = _model.eval()
+    except NotImplementedError as e:
+        print(str(e))
+        return
     
     if verbose:
-        print("Loading model weights at %s" % weights_fpath)
-    checkpoint = torch.load(weights_fpath, _device)
-    _model.load_state_dict(checkpoint['model_state'])
-    _model.eval()
+        print("Loaded synthesizer of model '%s' at path '%s'." % (model_type, weights_fpath))
+        print("Model has been trained to step %d." % (_model.state_dict()["step"]))
 
 
 def is_loaded():
     return _model is not None
 
 
-def infer_waveform(mel, normalize=True,  batched=True, target=hp_wavernn.gen_target, overlap=hp_wavernn.gen_overlap,
-                   progress_callback=None):
+def infer_waveform(mel, normalize=True,  batched=True, target=None, overlap=None, progress_callback=None):
     """
     Infers the waveform of a mel spectrogram output by the synthesizer (the format must match 
     that of the synthesizer!)
@@ -54,9 +50,21 @@ def infer_waveform(mel, normalize=True,  batched=True, target=hp_wavernn.gen_tar
     :param overlap: 
     :return: 
     """
-    if _model is None:
+    if _model is None or _model_type is None:
         raise Exception("Please load Wave-RNN in memory before using it")
-    
+
+    if _model_type == base.MODEL_TYPE_FATCHORD:
+        hp_wavernn = wavernn_fatchord
+    elif _model_type == base.MODEL_TYPE_GENEING:
+        hp_wavernn = wavernn_geneing
+    else:
+        raise NotImplementedError("Invalid model of type '%s' provided. Aborting..." % _model_type)
+
+    if target is None:
+        target = hp_wavernn.gen_target
+    if overlap is None:
+        overlap = hp_wavernn.gen_overlap
+
     if normalize:
         mel = mel / sp.max_abs_value
     mel = torch.from_numpy(mel[None, ...])
